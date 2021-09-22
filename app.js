c8e3387624df73d2e5fa0dd97c93966549bfb3ed
borshon420 <borshonarafat420@gmail.com>
2021-09-22 18:42:16 +0600

    practice-devtool

---
+++ app.js
@@ -0,0 +1,29 @@
+// const getMail = () => {
+//     const inputField = document.getElementById('user-mail')
+//     validateEmail()
+//     return inputField;
+    
+// }
+
+// function validateEmail(email) {
+//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
+
+// console.log(validateEmail())
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
